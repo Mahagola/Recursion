76efccb56bdfb643db8a40fd8085d8a328b46d72
Mahagola <suryanshyadav885@gmail.com>
2022-10-14 10:02:01 +0530

    Basics

---
+++ 03.cpp
@@ -0,0 +1,17 @@
+// printing linearly without using (i+1) n times
+#include<iostream>
+using namespace std;
+void func(int i, int n){
+    if(i<1){
+        return;
+    }
+    func(i-1,n);
+    cout<<i<<endl;
+
+}
+int main(){
+   int n;
+   cin>>n; 
+   func(n,n);
+   return 0;
+}
